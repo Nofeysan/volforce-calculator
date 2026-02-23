14de7a972a4b7ad058ae3cbec15563490692496f
Nofeysan <jxiaotishan@gmail.com>
2026-02-23 10:59:35 +0900

    論理式の修正

--- main.js
+++ main.js
@@ -102,16 +102,20 @@ client.on('interactionCreate', async interaction => {
         const vf = Math.floor( lv * (sc / 10000000) * gradeFactor * medalFactor * 20 ); 
 
         // 色設定
+        //let embedColor = 0xa52a2a
+
+        //*
         let embedColor = 0xa52a2a;
-        if (200 <= vf < 240) embedColor = 0x000080;
-        else if (240 <= vf < 280) embedColor = 0xfcc800;
-        else if (280 <= vf < 300) embedColor = 0x25b7c0;
-        else if (300 <= vf < 320) embedColor = 0xf73562;
-        else if (320 <= vf < 340) embedColor = 0xff69b4;
-        else if (340 <= vf < 360) embedColor = 0xd5ddef;
-        else if (360 <= vf < 380) embedColor = 0xffd700;
-        else if (380 <= vf < 400) embedColor = 0xff0000;
+        if (200 <= vf & vf < 240) embedColor = 0x000080;
+        else if (240 <= vf & vf < 280) embedColor = 0xfcc800;
+        else if (280 <= vf & vf < 300) embedColor = 0x25b7c0;
+        else if (300 <= vf & vf < 320) embedColor = 0xf73562;
+        else if (320 <= vf & vf < 340) embedColor = 0xff69b4;
+        else if (340 <= vf & vf < 360) embedColor = 0xd5ddef;
+        else if (360 <= vf & vf < 380) embedColor = 0xffd700;
+        else if (380 <= vf & vf < 400) embedColor = 0xff0000;
         else if (400 <= vf) embedColor = 0x800080;
+        //*/
 
         // 結果の返信
         const embed = new EmbedBuilder()
