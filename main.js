f5e9426fbaa94da7f06e5bf4c971ecb7e1d09b1a
Nofeysan <jxiaotishan@gmail.com>
2026-02-23 14:00:29 +0900

    使えない文字が含まれていたのを修正

--- main.js
+++ main.js
@@ -19,11 +19,11 @@ const commands = [
         .setName('sdvx')
         .setDescription('単曲VOLFORCEを計算します')
         .addNumberOption(opt => 
-            opt.setName('◆ Level ◆').setDescription('譜面定数を入力').setRequired(true).setMinValue(1.0).setMaxValue(20.9))
+            opt.setName('* レベル').setDescription('譜面定数を入力').setRequired(true).setMinValue(1.0).setMaxValue(20.9))
         .addIntegerOption(opt => 
-            opt.setName('◆ Score ◆').setDescription('スコアを入力（0 ~ 10,000,000）').setRequired(true).setMinValue(0).setMaxValue(10000000))
+            opt.setName('* スコア').setDescription('スコアを入力（0 ~ 10,000,000）').setRequired(true).setMinValue(0).setMaxValue(10000000))
         .addStringOption(opt => 
-            opt.setName('◆ ゲージ・ランプ ◆').setDescription('ゲージや、UC/PUCのランプを入力')
+            opt.setName('* ゲージ・ランプ').setDescription('ゲージや、UC/PUCのランプを入力')
             .setRequired(true)
             .addChoices(
                 { name: 'PUC', value: 'puc' },
